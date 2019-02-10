e733557236591919fd15fb93fb8abc1dfe751322
Steve <swcooke@sentara.com>
2019-02-10 10:19:00 -0500

    Adding small vm, has public ip (not ideal)

--- main.tf
+++ main.tf
@@ -10,3 +10,20 @@ resource "google_compute_network" "testing-network" {
   description             = "testing out configuring new networks"
   auto_create_subnetworks = "true"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "testing-instance1"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-lts"
+    }
+  }
+
+  network_interface {
+    network       = "${google_compute_network.testing-network.self_link}"
+    access_config = {
+    }
+  }
+}
