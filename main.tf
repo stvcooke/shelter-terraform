675352039dccecd89dfee2fddf927fb3dc7d4120
Steve <swcooke@sentara.com>
2019-02-11 16:00:18 -0500

    adding dns maybe

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "google_compute_network" "testing-network" {
   auto_create_subnetworks = "true"
 }
 
-resource "google_compute_instance" "vm_instance" {
+resource "google_compute_instance" "testing-instance" {
   name         = "testing-instance1"
   machine_type = "f1-micro"
 
@@ -26,4 +26,18 @@ resource "google_compute_instance" "vm_instance" {
     access_config = {
     }
   }
+}
+
+resource "google_dns_record_set" "testing-instance-dns" {
+  name         = "vpn.${google_dns_managed_zone.testing-zone.dns_name}"
+  type         = "A"
+  ttl          = 300
+  managed_zone = "${google_dns_managed_zone.testing-zone.name}"
+  rrdatas      = ["${google_compute_instance.testing-instance.network_interface.0.access_config.0.nat_ip}"]
+}
+
+resource "google_dns_managed_zone" "testing-zone" {
+  name        = "testing-zone"
+  dns_name    = "${var.dns_zone}."
+  description = "Purchase the domain prior to this."
 }
